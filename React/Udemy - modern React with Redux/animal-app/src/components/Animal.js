7f26903c1f696711bb07dad2fec2b355cdc8aa4c
Marcin S <you@example.com>
2023-01-03 17:16:38 +0100

    Start next course

--- React/Udemy - modern React with Redux/animal-app/src/components/Animal.js
+++ React/Udemy - modern React with Redux/animal-app/src/components/Animal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import bird from './bird.svg';
 import cat from './cat.svg';
@@ -6,6 +6,7 @@ import cow from './cow.svg';
 import dog from './dog.svg';
 import gator from './gator.svg';
 import horse from './horse.svg';
+import heart from './heart.svg';
 
 const mapPropToSvg = {
     bird,
@@ -17,9 +18,17 @@ const mapPropToSvg = {
 }
 
 const Animal = ({type}) => {
+
+    const [click, setClick] = useState(0);
+
+    const clickHeart = () => {
+        setClick(click + 1)
+    }
+
     return (
         <div style={{ display: 'inline-block', maxWidth: '100px' }}>
             <img style={{ maxWidth: '100%' }} src={ mapPropToSvg[type] } alt="bla" />
+            <img onClick={clickHeart} style={{ width: 10 + 10 * click + 'px' }} src={ heart } alt="bla" />
         </div>
     );
 
