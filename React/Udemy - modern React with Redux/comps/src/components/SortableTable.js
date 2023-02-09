1092ef9ec2020b8979f64a5a762412c30dbcbb55
Marcin PC <bla@bla.com>
2023-02-09 22:55:21 +0100

    React finished sortable table

--- React/Udemy - modern React with Redux/comps/src/components/SortableTable.js
+++ React/Udemy - modern React with Redux/comps/src/components/SortableTable.js
@@ -5,23 +5,41 @@ const SortableTable = (props) => {
   const [sortBy, setSortBy] = useState(null);
   const [sortOrder, setSortOrder] = useState(null);
 
+  const { columns, rows } = props;
+
   const handleClick = (label) => {
     switch (sortOrder) {
       case null:
         setSortOrder("asc");
+        setSortBy(label);
         break;
       case "asc":
         setSortOrder("desc");
+        setSortBy(label);
         break;
       default:
         setSortOrder(null);
         break;
     }
-
-    console.log(sortOrder);
   };
 
-  const { columns } = props;
+  let sortedData = rows;
+  if (sortOrder && sortBy) {
+    const { sortValue } = columns.find((column) => column.label === sortBy);
+
+    sortedData = [...rows].sort((a, b) => {
+      const valueA = sortValue(a);
+      const valueB = sortValue(b);
+
+      const reverseOrder = sortOrder === "asc" ? 1 : -1;
+
+      if (typeof valueA === "string") {
+        return valueA.localeCompare(valueB) * reverseOrder;
+      } else {
+        return (valueA - valueB) * reverseOrder;
+      }
+    });
+  }
 
   const updatedConfig = columns.map((column) => {
     if (!column.sortValue) {
@@ -31,14 +49,12 @@ const SortableTable = (props) => {
     return {
       ...column,
       header: () => (
-        <th onClick={() => handleClick(column.label)}>
-          {column.label} sortable
-        </th>
+        <th onClick={() => handleClick(column.label)}>{column.label}</th>
       ),
     };
   });
 
-  return <Table {...props} columns={updatedConfig} />;
+  return <Table {...props} rows={sortedData} columns={updatedConfig} />;
 };
 
 export default SortableTable;
